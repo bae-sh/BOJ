8920e6386eec825dfeb3535de0cd5a8c4ce45b18
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-09-21 14:05:20 +0900

    .

---
+++ 타일링11727번.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.util.*;
+import java.io.*;
+public class 타일링11727번 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		int d[]=new int[n+1];
+		
+		for(int i=0;i<n+1;i++) {
+			if(i<=1) {
+				d[i]=1;
+				continue;
+			}
+			d[i]=(d[i-1]+2*d[i-2])%10007;
+			
+		}
+		System.out.println(d[n]);
+
+	}
+
+}
