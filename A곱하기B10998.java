af65188c56eff9db02af11a654cfdf2028e7d5e4
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-09-30 04:53:59 +0900

    .

---
+++ A곱하기B10998.java
@@ -0,0 +1,14 @@
+package 백준;
+import java.util.*;
+public class A곱하기B10998 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextInt();
+		double b=sc.nextInt();
+		double c=a/b;
+		System.out.println(c);
+
+	}
+
+}
