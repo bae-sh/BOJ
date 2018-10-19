10b77141a18cf9693e09eaf319f10776a0657e21
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-10-20 02:40:37 +0900

    .

---
+++ 사칙연산10869번.java
@@ -0,0 +1,16 @@
+package 백준;
+import java.util.*;
+public class 사칙연산10869번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
