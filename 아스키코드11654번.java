dc02a5548c65a00c544c479548d8133e5bccf55a
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-10-29 16:21:12 +0900

    .

---
+++ 아스키코드11654번.java
@@ -0,0 +1,12 @@
+package 백준;
+import java.util.*;
+public class 아스키코드11654번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		char c=a.charAt(0);
+		System.out.println((int)c);
+	}
+
+}
