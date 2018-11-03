411af8ef67288e516c54b7af68da493f9c9471c1
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-11-04 01:56:45 +0900

    .

---
+++ 초콜릿자르기2163번.java
@@ -0,0 +1,13 @@
+package 백준;
+import java.util.*;
+public class 초콜릿자르기2163번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(a*b-1);
+
+	}
+
+}
