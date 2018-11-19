a5b2d6d508f7edf9f2cc61e5a54394ce78c451aa
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-11-19 17:02:50 +0900

    .

---
+++ R2_3046번.java
@@ -0,0 +1,12 @@
+package 백준;
+import java.util.*;
+public class R2_3046번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(b*2-a);
+	}
+
+}
