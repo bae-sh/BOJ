da9c7d3b2ab0af6560161246f0ea2cd2e392a76a
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-11-27 21:16:02 +0900

    .

---
+++ 다각형의대각선3049번.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class 다각형의대각선3049번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a*(a-1)*(a-2)*(a-3)/24);
+	}
+
+}
