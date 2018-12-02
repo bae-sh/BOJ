b25f6222e6405494713f392e40c8e877253aa307
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-12-02 22:03:48 +0900

    .

---
+++ 마이크로소프트로고5338번.java
@@ -0,0 +1,15 @@
+package 백준;
+
+public class 마이크로소프트로고5338번 {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+		
+
+	}
+
+}
