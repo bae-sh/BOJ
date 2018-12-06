e57a7e7acaf5bfb4e16239fcd2203a9357816616
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-12-07 00:14:16 +0900

    .

---
+++ 사파리월드2420번.java
@@ -0,0 +1,13 @@
+package 백준;
+import java.util.*;
+public class 사파리월드2420번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		long a=sc.nextLong();
+		long b=sc.nextLong();
+		long c=Math.abs(a-b);
+		System.out.println(c);
+	}
+
+}
