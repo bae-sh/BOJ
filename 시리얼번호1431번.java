5f93ac0059b94604455f13e7ef6ea13a822f58a3
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-12-12 00:09:33 +0900

    .

---
+++ 시리얼번호1431번.java
@@ -0,0 +1,19 @@
+package 백준;
+import java.util.*;
+public class 시리얼번호1431번 implements Comparable{
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String d[]=new String[n];
+		
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		String a=this;
+		if()
+		return 0;
+	}
+
+}
