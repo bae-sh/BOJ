f5063051279dc469c9472db67919b211e2552900
DESKTOP-2FISH38\seong <b5460881@naver.com>
2019-01-25 15:35:30 +0900

    .

---
+++ 인접한비트의개수2698번.java
@@ -0,0 +1,17 @@
+package 백준;
+import java.util.*;
+public class 인접한비트의개수2698번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int dp[][]=new int[101][100];
+		for(int i=1;i<=100;i++) {
+		}
+	}
+	static int med(int dp[][],int a) {
+		if(dp[a+1][0]==1) {
+			
+		}
+	}
+
+}
