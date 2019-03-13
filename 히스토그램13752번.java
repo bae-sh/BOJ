0fa60f52d2e898b8e66af940d6654e0d9958306b
DESKTOP-2FISH38\seong <b5460881@naver.com>
2019-03-13 18:30:53 +0900

    .

---
+++ 히스토그램13752번.java
@@ -0,0 +1,17 @@
+package 백준;
+import java.util.*;
+public class 히스토그램13752번 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			int a=sc.nextInt();
+			for(int j=0;j<a;j++) {
+				System.out.print("=");
+			}
+			System.out.println();
+		}
+	}
+
+}
