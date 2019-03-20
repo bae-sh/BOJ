89d16430171568b58c412582c46203f62bdb6181
DESKTOP-2FISH38\seong <b5460881@naver.com>
2019-03-20 17:13:17 +0900

    .

--- FlymetotheAlphaCentauri1011번.java
+++ FlymetotheAlphaCentauri1011번.java
@@ -4,9 +4,21 @@ public class FlymetotheAlphaCentauri1011번 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		for(int i=0;i<n;i++) {
-			
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++) {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			double n=y-x;
+			int c=(int)Math.sqrt(n);
+			int ans=0;
+			if(c*c>n) {
+				c--;
+			}
+			ans+=2*c-1;
+			double k=n-c*c;
+			double k2=Math.ceil(k/c);
+			ans+=k2;
+			System.out.println(ans);
 		}
 	}
 
