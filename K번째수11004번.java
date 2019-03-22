b53a8fd69f091b55ae14d7a97f8ac027d769ffef
DESKTOP-2FISH38\seong <b5460881@naver.com>
2019-03-22 13:03:56 +0900

    .

--- K번째수11004번.java
+++ K번째수11004번.java
@@ -1,19 +1,43 @@
 package 백준;
 
 import java.util.*;
+import java.io.*;
 public class K번째수11004번 {
 
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int n=sc.nextInt();
-		int k=sc.nextInt();
-		int a[]=new int[n];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String[] st=br.readLine().split(" ");
+		int n=Integer.parseInt(st[0]);
+		int k=Integer.parseInt(st[1]);
+		ArrayList<Integer> a=new ArrayList<>();
+		st=br.readLine().split(" ");
 		for(int i=0;i<n;i++) {
-			a[i]=sc.nextInt();
+			a.add(Integer.parseInt(st[i]));
 		}
-		Arrays.sort(a);
-		System.out.println(a[k-1]);
-
+		System.out.println(quick_selection(a, k));
 	}
-
+	public static int quick_selection(ArrayList<Integer> arr,int kth) {
+		int pivot=arr.get(arr.size()/2);
+		ArrayList<Integer> left=new ArrayList<>();
+		ArrayList<Integer> mid=new ArrayList<>();
+		ArrayList<Integer> right=new ArrayList<>();
+		for(int i:arr) {
+			if(i<pivot) 
+				left.add(i);
+			else if(i>pivot) 
+				right.add(i);
+			else 
+				mid.add(i);
+			}
+			if(kth<=left.size()) {
+				return quick_selection(left, kth);
+			}
+			else if(kth<=left.size()+mid.size()) {
+				return mid.get(0);
+			}
+			else {
+				return quick_selection(right, kth-left.size()-mid.size());
+			}
+		
+	}
 }
