fc07268abfef0c312c0728b3a8b94854ac8229c1
DESKTOP-2FISH38\seong <b5460881@naver.com>
2020-09-16 10:10:23 +0900

    1145

---
+++ 1145.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int A[5];
+int main() {
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &A[i]);
+	}
+	for (int i = 1;; i++) {
+		int cnt = 0;
+		for (int j = 0; j < 5; j++) {
+			if (i%A[j] == 0) {
+				cnt++;
+			}
+		}
+		if (cnt >= 3) {
+			printf("%d", i);
+			break;
+		}
+	}
+}
