124a675f4e53fe1263c1367a1e095f49db2feb86
DESKTOP-2FISH38\seong <b5460881@naver.com>
2020-12-08 15:04:14 +0900

    플로와샬,mst

--- 1412/1412/1412.cpp
+++ 1412/1412/1412.cpp
@@ -1,59 +1,39 @@
 #include<iostream>
-#include<vector>
-#include<queue>
+#include<cstring>
 using namespace std;
+char c[100][100];
+bool d[100][100];
 int N;
-vector<int> v[100];
-vector<pair<int, int>> v2;//양방향
-char map[100][100];
-queue<pair<int,int>> q;//자신,부모
-bool impossible;
-int indegree[100];
-int parent[100][100];//자신,부모,몇대손
-
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			char c; cin >> c;
-			map[i][j] = c;
+			cin >> c[i][j];
 		}
 	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			if (map[i][j] == 'Y') {
-				if (map[j][i] == 'Y' && i <= j) {
-					v2.push_back({ i,j });
-				}
-				else if (map[j][i] == 'N') {
-					v[i].push_back(j);
-					indegree[j]++;
-				}
+			if (c[i][j] == 'Y' && c[j][i] == 'N') {
+				d[i][j] = true;
 			}
 		}
 	}
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (d[i][k] && d[k][j]) d[i][j] = true;
+			}
+		}
+	}
+
 	for (int i = 0; i < N; i++) {
-		if (indegree[i] == 0) {
-			q.push({ i,i });
-			parent[i][i] = 1;
+		if (d[i][i]) {
+			cout << "NO\n";
+			return 0;
 		}
 	}
-	
-	while (!q.empty()) {
-		int cur = q.front().first; 
-		int p = q.front().second; q.pop();
-		for (int i = 0; i < v[cur].size(); i++) {
-			int next = v[cur][i];
-			if (parent[next][p] != 0) {
-				cout << "NO";
-				return 0;
-			}
-			else {
-				parent[next][p] = parent[cur][p] + 1;
-				q.push({ next,p });
-			}
-		}
-	}
+	cout << "YES\n";
+	return 0;
 }
--- 1647/1647/1647.cpp
+++ 1647/1647/1647.cpp
@@ -1,28 +1,44 @@
 #include<iostream>
 #include<vector>
-
+#include<algorithm>
 using namespace std;
-int N;
-int parent[100001];
 
+int N, M, cnt, ans;
+int p[100001];
+vector<pair<int,pair<int, int>>> v;
 int Find(int x) {
-	if (parent[x] == 0) {
-		return 0;
-	}
-	else if (parent[x] == x) {
-		return x;
-	}
-	else {
-		return parent[x] = Find(x);
-	}
+	if (p[x] == x) return x;
+	else return p[x] = Find(p[x]);
 }
 void Union(int x, int y) {
 	int parent_x = Find(x);
 	int parent_y = Find(y);
-	if (parent_x == 0) {
-
-	}
+	p[parent_x] = parent_y;
 }
 int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int a, b, c; cin >> a >> b >> c;
+		v.push_back({ c,{a,b} });
+	}
+	for (int i = 0; i <= N; i++) {
+		p[i] = i;
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		int cost = v[i].first;
+		int from = v[i].second.first;
+		int to = v[i].second.second;
+		if (Find(from) != Find(to)) {
+			Union(from, to);
+			cnt++;
+			ans += cost;
+		}
+		if (cnt == N - 2) {
+			cout << ans << "\n";
+			return 0;
+		}
+	}
 }
