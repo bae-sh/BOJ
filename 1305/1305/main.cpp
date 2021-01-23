fdd8ec54a167d1ca2170680b560ce1fbebc049ba
bae-sh <b5460881@naver.com>
2021-01-23 15:00:55 +0900

    .

--- 1305/1305/main.cpp
+++ 1305/1305/main.cpp
@@ -4,23 +4,24 @@
 using namespace std;
 int L;
 string orgin;
-vector<int> pi(L,0);
-void getPi(string s){
+vector<int> getPi(string s){
     int m=s.size(); int j=0;
+    vector<int> ret(L,0);
     for(int i=1;i<s.size();i++){
         while(j>0&&s[i]!=s[j]){
-            j=pi[j-1];
+            j=ret[j-1];
         }
         if(s[i]==s[j]){
-            pi[i]=++j;
+            ret[i]=++j;
         }
     }
+    return ret;
 }
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     
     cin>>L>>orgin;
-    getPi(orgin);
+    auto pi=getPi(orgin);
     cout<<L-pi[L-1]<<"\n";
     return 0;
 }
