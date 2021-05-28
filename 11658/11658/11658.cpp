8879bb8d98e39281a111faa500a3d155ff2bd5b4
bas-sh <b5460881@naver.com>
2021-05-29 01:45:09 +0900

    펜윅트리

--- 11658/11658/11658.cpp
+++ 11658/11658/11658.cpp
@@ -1,45 +1,52 @@
-#include <cstdio>
+#include <iostream>
+#include<algorithm>
+using namespace std;
 int n, m;
 int a[1025][1025];
 int tree[1025][1025];
-void update(int x, int y, int val) {
-    for (int i = x; i <= n; i += i & -i) {
-        for (int j = y; j <= n; j += j & -j) {
-            tree[i][j] += val;
-        }
-    }
+int sum(int x,int y) {
+	int ans = 0;
+	while (x > 0) {
+		int ny = y;
+		while (ny > 0) {
+			ans += tree[x][ny];
+			ny -= (ny & -ny);
+		}
+		x -= (x & -x);
+	}
+	return ans;
 }
-int sum(int x, int y) {
-    int ans = 0;
-    for (int i = x; i > 0; i -= i & -i) {
-        for (int j = y; j > 0; j -= j & -j) {
-            ans += tree[i][j];
-        }
-    }
-    return ans;
+void update(int x, int y, int diff) {
+	while (x <= n) {
+		int ny = y;
+		while (ny <= n) {
+			tree[x][ny] += diff;
+			ny += (ny & -ny);
+		}
+		x += (x & -x);
+	}
 }
 int main() {
-    scanf("%d %d", &n, &m);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            scanf("%d", &a[i][j]);
-            update(i, j, a[i][j]);
-        }
-    }
-    while (m--) {
-        int what;
-        scanf("%d", &what);
-        if (what == 0) {
-            int x, y, val;
-            scanf("%d %d %d", &x, &y, &val);
-            update(x, y, val - a[x][y]);
-            a[x][y] = val;
-        }
-        else {
-            int x1, y1, x2, y2;
-            scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-            printf("%d\n", sum(x2, y2) - sum(x1 - 1, y2) - sum(x2, y1 - 1) + sum(x1 - 1, y1 - 1));
-        }
-    }
-    return 0;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> a[i][j];
+			update(i, j, a[i][j]);
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		int w; cin >> w;
+		if (w == 0) {
+			int x, y, c; cin >> x >> y >> c;
+			int diff = c - a[x][y];
+			a[x][y] = c;
+			update(x, y, diff);
+		}
+		else {
+			int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+			cout << sum(x2, y2) - sum(x1 - 1, y2) - sum(x2, y1 - 1) + sum(x1 - 1, y1 - 1) << "\n";
+		}
+	}
 }
