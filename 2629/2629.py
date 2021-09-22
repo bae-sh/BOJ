13f68d13aac02ce2aa4138e6f731464829f42a77
bae-sh <hyeon1293@gmail.com>
2021-09-23 00:05:28 +0900

    dp

--- 2629/2629.py
+++ 2629/2629.py
@@ -1,5 +1,25 @@
-import numpy as np
+N = int(input())
+N_list = list(map(int, input().split()))
+M = int(input())
+M_list = list(map(int, input().split()))
+dp = [False for i in range(40001)]
+dp[0] = True
+for cur in N_list:
+    ans = list()
+    for j in range(40000, 0, -1):
+        if dp[j] and cur-j >= 0:
+            ans.append(cur-j)
+        if dp[j] and j-cur >= 0:
+            ans.append(j-cur)
+        if j-cur >= 0 and dp[j-cur]:
+            ans.append(j)
 
-a = np.zeros(40001)
+    for j in ans:
+        dp[j] = True
 
-print(a)
+
+for i in M_list:
+    if dp[i]:
+        print("Y", end=" ")
+    else:
+        print("N", end=" ")
