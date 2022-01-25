dd49759aa99c7ade0114dd2aea15b8521cf6dc56
bae-sh <hyeon1293@gmail.com>
2022-01-25 19:12:23 +0900

    greedy

---
+++ 13305/13305.js
@@ -0,0 +1,12 @@
+var fs = require("fs"); //"/dev/stdin"
+var input = fs.readFileSync("input.txt").toString().split("\n");
+let distance = input[1].split(" ").map((v) => BigInt(v));
+let costs = input[2].split(" ").map((v) => BigInt(v));
+let minCost = costs[0];
+let ans = 0n;
+for (let i = 0; i < distance.length; i++) {
+    let dis = distance[i];
+    ans += dis * minCost;
+    minCost = minCost > costs[i + 1] ? costs[i + 1] : minCost;
+}
+console.log(String(ans));
