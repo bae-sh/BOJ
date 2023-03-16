b19d6f5af1044b772bf1616b2a9b4a27b393cb53
bae-sh <hyeon1293@gmail.com>
2023-03-16 17:37:51 +0900

    math

---
+++ 2477/2477.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split('\n');
+
+const arr = [];
+let area = 0;
+const k = +input.shift();
+
+for (let i = 0; i < 6; i++) {
+  const [a, b] = input.shift().split(' ').map(Number);
+  arr.push({ a, b });
+}
+
+arr.push(...arr);
+console.log(arr);
+for (let i = 0; i < 6; i++) {
+  if (arr[i].a === arr[i + 2].a && arr[i + 1].a === arr[i + 3].a) {
+    area = (arr[i].b + arr[i + 2].b) * (arr[i + 1].b + arr[i + 3].b) - arr[i + 1].b * arr[i + 2].b;
+    break;
+  }
+}
+
+console.log(area * k);
