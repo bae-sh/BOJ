d57e157e575a08bc4280ce7dc04660eaaf699465
bae-sh <hyeon1293@gmail.com>
2023-06-20 13:32:33 +0900

    union

--- 1647/1647/1647.cpp
+++ 1647/1647/1647.cpp
@@ -1,42 +1,60 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int N, M, cnt, ans;
 int p[100001];
-vector<pair<int,pair<int, int>>> v;
-int Find(int x) {
-	if (p[x] == x) return x;
-	else return p[x] = Find(p[x]);
+vector<pair<int, pair<int, int>>> v;
+int Find(int x)
+{
+	if (p[x] == x)
+		return x;
+	else
+		return p[x] = Find(p[x]);
 }
-void Union(int x, int y) {
+void Union(int x, int y)
+{
 	int parent_x = Find(x);
 	int parent_y = Find(y);
 	p[parent_x] = parent_y;
 }
-int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int a, b, c; cin >> a >> b >> c;
-		v.push_back({ c,{a,b} });
+	for (int i = 0; i < M; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		v.push_back({c, {a, b}});
 	}
-	for (int i = 0; i <= N; i++) {
+	for (int i = 0; i <= N; i++)
+	{
 		p[i] = i;
 	}
 	sort(v.begin(), v.end());
-	for (int i = 0; i < v.size(); i++) {
+	for (int i = 0; i < v.size(); i++)
+	{
 		int cost = v[i].first;
 		int from = v[i].second.first;
 		int to = v[i].second.second;
-		if (Find(from) != Find(to)) {
+		if (cnt == N - 2)
+		{
+			cout << ans << "\n";
+			return 0;
+		}
+		if (Find(from) != Find(to))
+		{
 			Union(from, to);
 			cnt++;
 			ans += cost;
 		}
-		if (cnt == N - 2) {
+		if (cnt == N - 2)
+		{
 			cout << ans << "\n";
 			return 0;
 		}
