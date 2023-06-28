58fce73ad9e949eccebdc9c9622d3af35b45320a
bae-sh <hyeon1293@gmail.com>
2023-06-29 02:05:10 +0900

    dfs

--- 2250/2250.js
+++ 2250/2250.js
@@ -4,14 +4,22 @@ const input = fs.readFileSync('input.txt').toString().split('\n');
 const N = +input.shift();
 
 const arr = Array.from({ length: N + 1 }, () => new Array());
+const arrReverse = Array.from({ length: N + 1 }, () => new Array());
 const levelArr = Array.from({ length: N + 1 }, () => new Array());
 for (let i = 0; i < N; i++) {
   const [a, b, c] = input.shift().split(' ').map(Number);
   arr[a].push(b, c);
+  if (b !== -1) arrReverse[b].push(a);
+  if (c !== -1) arrReverse[c].push(a);
 }
-let count = 1;
-let [ansLevel, ansWidth] = [1, 1];
 
+let count = 1;
+let [ansLevel, ansWidth] = [0, 0];
+
+function findParent(num) {
+  if (arrReverse[num].length === 0) return num;
+  return findParent(arrReverse[num]);
+}
 function check(num, level) {
   const [l, r] = arr[num];
   if (l !== -1) {
@@ -26,7 +34,9 @@ function check(num, level) {
   return;
 }
 
-check(1, 1);
+const start = findParent(1);
+
+check(start, 1);
 
 for (let i = 1; i < N + 1; i++) {
   if (levelArr[i].length === 0) break;
