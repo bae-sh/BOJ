af72967612468eaaef54afd871816b70b0fa8485
bae-sh <hyeon1293@gmail.com>
2023-06-29 20:31:05 +0900

    stack

---
+++ 17299/17299.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split('\n');
+
+const N = +input.shift();
+const arr = input.shift().split(' ').map(Number).reverse();
+const ans = [];
+const st = [];
+
+const counts = new Array(1_000_001).fill(0);
+
+arr.forEach(item => {
+  counts[item] += 1;
+});
+
+for (const item of arr) {
+  while (st.length !== 0) {
+    const topItem = st[st.length - 1];
+    if (counts[item] < counts[topItem]) {
+      st.push(item);
+      ans.push(topItem);
+      break;
+    } else {
+      st.pop();
+    }
+  }
+
+  if (st.length === 0) {
+    st.push(item);
+    ans.push(-1);
+  }
+}
+
+console.log(ans.reverse().join(' '));
